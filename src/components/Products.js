5d3aef3e6e41a971cadbfb70bbb410281eac6b8e
Omama Zainab <omi.zainy@gmail.com>
2020-07-11 11:48:46 +0500

    updated product.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import shoes from '../shoes.json'
+import { Link } from 'react-router-dom'
+import './../App.css'
 
 const Products = () => {
+
     return (
-        <div>
-            products
+        <div className="product-container">
+
+            {Object.keys(shoes).map(keyName => {
+                const shoe = shoes[keyName]
+                return (
+                        <Link
+                            className="Link"
+                            to={`/products/${keyName}`}
+                            key={keyName}
+                        >
+                            <div >
+                                <img src={shoe.img} alt="shoe" width='100%' />
+                                <h6 className='black-text'>{shoe.name}</h6>
+
+                            </div>
+
+                        </Link>
+
+
+
+                )
+            }
+            )}
         </div>
     )
 }
